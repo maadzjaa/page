b579b19a83a143d5ac2aaa710af44da85f8827cf
Magdalena <magda.g2112@gmail.com>
2023-11-27 21:27:46 +0100

    feat: slider

--- main.js
+++ main.js
@@ -6,4 +6,27 @@ navButton.addEventListener('click', () => {
 	navList.classList.toggle('open');
 });
 
-console.log(navList);
+const slider = document.querySelector('.reviews');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+	isDown = true;
+	slider.classList.add('active');
+});
+
+slider.addEventListener('mouseleave', () => {
+	isDown = false;
+	slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+	isDown = false;
+	slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', () => {
+	if (!isDown) return;
+	console.count(isDown);
+});
